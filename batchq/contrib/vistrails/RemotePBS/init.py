63a555156baa97ef286ce2753fce1415ccd5d726
Tommy Ellqvist <tommy.ellqvist@nyu.edu>
2014-02-18 17:58:22 +0100

    Updated RemotePBS to support Job Monitor on VisTrails master branch

--- batchq/contrib/vistrails/RemotePBS/init.py
+++ batchq/contrib/vistrails/RemotePBS/init.py
@@ -1,5 +1,6 @@
-from core.modules.vistrails_module import Module, ModuleError, ModuleSuspended
-from core.system import current_user
+from vistrails.core.modules.vistrails_module import Module, ModuleError, ModuleSuspended
+from vistrails.core.system import current_user
+from vistrails.core.interpreter.job import JobMixin, JobMonitor
 from batchq.pipelines.shell import FileCommander as BQMachine
 from batchq.core.stack import select_machine, end_machine, use_machine, \
                                                                 current_machine
@@ -7,6 +8,8 @@ from batchq.batch.commandline import PBS, PBSScript
 from batchq.batch.directories import CreateDirectory
 from batchq.batch.files import TransferFiles
 from batchq.pipelines.shell.ssh import SSHTerminal
+
+import hashlib
 
 class Machine(Module, BQMachine):
     _input_ports = [('server', '(edu.utah.sci.vistrails.basic:String)', True),
@@ -44,7 +47,6 @@ Machine._output_ports = [('value', Machine)]
 class RunCommand(Module):
     _input_ports = [('machine', Machine),
                     ('command', '(edu.utah.sci.vistrails.basic:String)', True),
-                    ('cacheable', '(edu.utah.sci.vistrails.basic:Boolean)', True),
                    ]
     
     _output_ports = [('machine', Machine),
@@ -52,25 +54,25 @@ class RunCommand(Module):
                     ]
     
     def compute(self):
-        self.is_cacheable = lambda *args, **kwargs: False
         if not self.hasInputFromPort('machine'):
             raise ModuleError(self, "No machine specified")
         if not self.hasInputFromPort('command'):
             raise ModuleError(self, "No command specified")
         command = self.getInputFromPort('command').strip()
         machine = self.getInputFromPort('machine').machine
-        cacheable = False
-        if self.hasInputFromPort('cacheable'):
-            cacheable = self.getInputFromPort('cacheable')
 
-        ## This indicates that the coming commands submitted on the machine
-        # trick to select machine without initializing every time
-        use_machine(machine)
-        m = current_machine()
-        result = m.remote.send_command(command)
-        end_machine()
-        if cacheable:
-            self.is_cacheable = lambda *args, **kwargs: True
+        jm = JobMonitor.getInstance()
+        cache = jm.getCache(self.signature)
+        if cache:
+            result = cache['result']
+        else:
+            ## This indicates that the coming commands submitted on the machine
+            # trick to select machine without initializing every time
+            use_machine(machine)
+            m = current_machine()
+            result = m.remote.send_command(command)
+            end_machine()
+            cache = jm.setCache(self.signature, {'result':result})
         self.setResult("output", result)
         self.setResult("machine", self.getInputFromPort('machine'))
 
@@ -154,7 +156,7 @@ class PBSJob(Module):
         self.setResult("file_list",
                        [f.split(' ')[-1] for f in files.split('\n')[1:]])
 
-class RunPBSScript(Module):
+class RunPBSScript(JobMixin,Module):
     _input_ports = [('machine', Machine),
                     ('command', '(edu.utah.sci.vistrails.basic:String)', True),
                     ('working_directory', '(edu.utah.sci.vistrails.basic:String)'),
@@ -171,65 +173,72 @@ class RunPBSScript(Module):
                      ('stderr', '(edu.utah.sci.vistrails.basic:String)'),
                     ]
     
-    def compute(self):
-        self.is_cacheable = lambda *args, **kwargs: False
+    def getId(self, params):
+        return hashlib.md5(params['input_directory'] +
+                           params['command'] +
+                           params['working_directory']).hexdigest()
+        
+    def readInputs(self):
+        self.job = None
+        d = {}
         if not self.hasInputFromPort('machine'):
             raise ModuleError(self, "No machine specified")
-        machine = self.getInputFromPort('machine').machine
+        self.machine = self.getInputFromPort('machine').machine
         if not self.hasInputFromPort('command'):
             raise ModuleError(self, "No command specified")
-        command = self.getInputFromPort('command').strip()
-        working_directory = self.getInputFromPort('working_directory') \
+        d['command'] = self.getInputFromPort('command').strip()
+        d['working_directory'] = self.getInputFromPort('working_directory') \
               if self.hasInputFromPort('working_directory') else '.'
         if not self.hasInputFromPort('input_directory'):
             raise ModuleError(self, "No input directory specified")
-        input_directory = self.getInputFromPort('input_directory').strip()
-        additional_arguments = {'processes': 1, 'time': -1, 'mpi': False,
+        d['input_directory'] = self.getInputFromPort('input_directory').strip()
+        d['additional_arguments'] = {'processes': 1, 'time': -1, 'mpi': False,
                                 'threads': 1, 'memory':-1, 'diskspace': -1}
-        for k in additional_arguments:
+        for k in d['additional_arguments']:
             if self.hasInputFromPort(k):
-                additional_arguments[k] = self.getInputFromPort(k)
-        ## This indicates that the coming commands submitted on the machine
-        # trick to select machine without initializing every time
+                d['additional_arguments'][k] = self.getInputFromPort(k)
+        return d
 
-        use_machine(machine)
-        cdir = CreateDirectory("remote", working_directory)
-        trans = TransferFiles("remote", input_directory, working_directory,
-                              dependencies = [cdir])
-        job = PBSScript("remote", command, working_directory,
-                        dependencies = [trans], **additional_arguments)
-        job.run()
+    def startJob(self, params):
+        work_dir = params['working_directory']
+        use_machine(self.machine)
+        self.cdir = CreateDirectory("remote", work_dir)
+        trans = TransferFiles("remote", params['input_directory'], work_dir,
+                              dependencies = [self.cdir])
+        self.job = PBSScript("remote", params['command'], work_dir,
+                      dependencies = [trans], **params['additional_arguments'])
+        self.job.run()
         try:
-            ret = job._ret
+            ret = self.job._ret
             if ret:
                 job_id = int(ret)
         except ValueError:
             end_machine()
             raise ModuleError(self, "Error submitting job: %s" % ret)
-        finished = job.finished()
-        job_info = job.get_job_info()
-        if job_info:
-            self.annotate({'job_info': job.get_job_info()})
-        if not finished:
-            status = job.status()
-            # try to get more detailed information about the job
-            # this only seems to work on some versions of torque
-            if job_info:
-                comment = [line.strip() for line in job_info.split('\n')
-                           if line.strip().startswith('comment =')]
-                if comment:
-                    status += ': ' + comment[0][10:]
-            end_machine()
-            raise ModuleSuspended(self, str(status), queue=job)
-        self.is_cacheable = lambda *args, **kwargs: True
-        # copies the created files to the client
-        get_result = TransferFiles("local", input_directory, working_directory,
-                              dependencies = [cdir])
-        get_result.run()
+        return params
+        
+    def getMonitor(self, params):
+        if not self.job:
+            self.startJob(params)
+        return self.job
 
+    def finishJob(self, params):
+        job_info = self.job.get_job_info()
+        if job_info:
+            self.annotate({'job_info': job_info})
+        # copies the created files to the client
+        get_result = TransferFiles("local", params['input_directory'],
+                                   params['working_directory'],
+                                   dependencies = [self.cdir])
+        get_result.run()
         end_machine()
-        self.setResult("stdout", job.standard_output())
-        self.setResult("stderr", job.standard_error())
+        stdout = self.job.standard_output()
+        stderr = self.job.standard_error()
+        return {'stdout':stdout, 'stderr':stderr}
+
+    def setResults(self, params):
+        self.setResult('stdout', params['stdout'])
+        self.setResult('stderr', params['stderr'])
 
 class SyncDirectories(Module):
     _input_ports = [('machine', Machine),
@@ -257,18 +266,21 @@ class SyncDirectories(Module):
             whereto = 'local'
 
 
-        ## This indicates that the coming commands submitted on the machine
-        # trick to select machine without initializing every time
+        jm = JobMonitor.getInstance()
+        cache = jm.getCache(self.signature)
+        if not cache:
+            ## This indicates that the coming commands submitted on the machine
+            # trick to select machine without initializing every time
 
-        use_machine(machine)
-        to_dir = local_directory if whereto=='local' else remote_directory
-        cdir = CreateDirectory(whereto, to_dir)
-        job = TransferFiles(whereto, local_directory, remote_directory,
+            use_machine(machine)
+            to_dir = local_directory if whereto=='local' else remote_directory
+            cdir = CreateDirectory(whereto, to_dir)
+            job = TransferFiles(whereto, local_directory, remote_directory,
                               dependencies = [cdir])
-        job.run()
-        self.is_cacheable = lambda *args, **kwargs: True
+            job.run()
+            end_machine()
+            cache = jm.setCache(self.signature, {'result':''})
 
-        end_machine()
         self.setResult("machine", machine)
 
 class CopyFile(Module):
@@ -283,7 +295,6 @@ class CopyFile(Module):
                     ]
     
     def compute(self):
-        self.is_cacheable = lambda *args, **kwargs: False
         if not self.hasInputFromPort('machine'):
             raise ModuleError(self, "No machine specified")
         machine = self.getInputFromPort('machine').machine
@@ -297,13 +308,16 @@ class CopyFile(Module):
         if self.hasInputFromPort('to_local') and self.getInputFromPort('to_local'):
             whereto = 'local'
 
-
-        ## This indicates that the coming commands submitted on the machine
-        # trick to select machine without initializing every time
-
-        command = machine.getfile if whereto=='local' else machine.sendfile
-        result = command(local_file, remote_file)
-        self.is_cacheable = lambda *args, **kwargs: True
+        jm = JobMonitor.getInstance()
+        cache = jm.getCache(self.signature)
+        if cache:
+            result = cache['result']
+        else:
+            ## This indicates that the coming commands submitted on the machine
+            # trick to select machine without initializing every time
+            command = machine.getfile if whereto=='local' else machine.sendfile
+            result = command(local_file, remote_file)
+            cache = jm.setCache(self.signature, {'result':result})
 
         self.setResult("machine", self.getInputFromPort('machine'))
         self.setResult("output", result)
